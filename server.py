21e55658401fc2b37478b028af11749a572cee6b
Trevor Riley <triley.freelancer@gmail.com>
2024-10-27 00:15:11 -0400

    updated for static code analysis improvements

--- server.py
+++ server.py
@@ -7,6 +7,9 @@ app = Flask('Emotion Detector')
 
 @app.route('/emotionDetector')
 def get_emotion_detection():
+    ''' GET request, processes text input with emotion detector
+        and gives response as a formatted string
+    '''
     # get the text to be analyzed from the request args
     text_to_analyze = request.args.get('textToAnalyze')
 
@@ -16,24 +19,23 @@ def get_emotion_detection():
     # error handling
     if response['dominant_emotion'] is None:
         return "Invalid text! Please try again!."
-
     # create a formatted string to return
     # the string is really long, so I spit it up over multiple lines
-    formatted_str = (f"For the given statement, the system response is " 
+    formatted_str = (f"For the given statement, the system response is "
         f"'anger': {response['anger']}, " 
         f"'disgust': {response['disgust']}, " 
         f"'fear': {response['fear']}, " 
         f"'joy': {response['joy']} " 
         f"and 'sadness': {response['sadness']}. " 
         f"The dominant emotion is {response['dominant_emotion']}.")
-
     return formatted_str
 
-''' renders the index page
-'''
+
 @app.route("/")
 def render_index_page():
+    ''' renders the index page
+    '''
     return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5002)  
+    app.run(host="0.0.0.0", port=5002)
